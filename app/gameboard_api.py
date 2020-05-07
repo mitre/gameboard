d7934c24579276eb87ed3e482aed1ba71bcdc197
mchan143 <61020544+mchan143@users.noreply.github.com>
2020-05-07 11:10:05 -0400

    discussed changes and minor fixes

--- app/gameboard_api.py
+++ app/gameboard_api.py
@@ -6,6 +6,10 @@ from app.utility.base_service import BaseService
 
 
 class GameboardApi(BaseService):
+    RED_TEAM = 'red'
+    BLUE_TEAM = 'blue'
+    PID_FACT = 'host.process.id'
+    AUTOCOLLECT_NAME = 'Auto-Collect'
 
     def __init__(self, services):
         self.auth_svc = services.get('auth_svc')
@@ -21,10 +25,10 @@ class GameboardApi(BaseService):
     @check_authorization
     async def get_pieces(self, request):
         data = dict(await request.json())
-        red_op = await self.data_svc.locate('operations', dict(id=data.get('red')))
-        blue_op = await self.data_svc.locate('operations', dict(id=data.get('blue')))
+        red_op = await self.data_svc.locate('operations', dict(id=data.get(self.RED_TEAM)))
+        blue_op = await self.data_svc.locate('operations', dict(id=data.get(self.BLUE_TEAM)))
         access = await self.auth_svc.get_permissions(request)
-        response = dict(access='blue' if self.Access.BLUE in access else 'red',
+        response = dict(access=self.BLUE_TEAM if self.Access.BLUE in access else self.RED_TEAM,
                         red_op=red_op[0].display if red_op else None,
                         blue_op=blue_op[0].display if blue_op else None,
                         exchanges=self._get_exchanges(red_op, blue_op))
@@ -32,38 +36,39 @@ class GameboardApi(BaseService):
 
     def _get_exchanges(self, red_ops, blue_ops):
         exchanges = dict()
-        red_links = self._get_sorted_links(red_ops)
-        blue_links = self._get_sorted_links(blue_ops)
-        for link in red_links:
-            if link.pid in exchanges.keys():
-                exchanges[link.pid]['red'].append(link.display)
-            else:
-                exchanges[link.pid] = dict(red=[link.display], blue=[])
-        if blue_ops and 'Auto-Collect' not in blue_ops[0].name:
-            self._set_pins(blue_ops[0])
-        for link in blue_links:
-            if link.pin in exchanges.keys():
-                exchanges[link.pin]['blue'].append(link.display)
-            else:
-                exchanges[link.pin] = dict(red=[], blue=[link.display])
+        self._set_pins(blue_ops)
+        exchanges = self._add_links_to_exchange(exchanges, self._get_sorted_links(red_ops), team=self.RED_TEAM)
+        exchanges = self._add_links_to_exchange(exchanges, self._get_sorted_links(blue_ops), team=self.BLUE_TEAM)
         return list(exchanges.items())
 
-    def _set_pins(self, blue_op):
-        for lnk in blue_op.chain:
-            fact = next((f for f in lnk.used), None)
-            if fact:
-                if fact.trait == 'host.process.id':
-                    lnk.pin = int(fact.value)
+    def _set_pins(self, blue_ops):
+        if blue_ops and self.AUTOCOLLECT_NAME not in blue_ops[0].name:
+            for lnk in blue_ops[0].chain:
+                fact = next((f for f in lnk.used), None)
+                if fact:
+                    if fact.trait == self.PID_FACT:
+                        lnk.pin = int(fact.value)
+                    else:
+                        lnk.pin = self._find_original_pid(blue_ops[0].all_relationships(), fact.trait, fact.value)
+
+    def _add_links_to_exchange(self, exchanges, links, team):
+        for link in links:
+            key = link.pid if team == self.RED_TEAM else link.pin
+            if key in exchanges.keys():
+                exchanges[key][team].append(link.display)
+            else:
+                if team == self.RED_TEAM:
+                    exchanges[key] = dict(red=[link.display], blue=[])
                 else:
-                    lnk.pin = self._find_original_pid(blue_op.all_relationships(), fact.trait, fact.value)
+                    exchanges[key] = dict(red=[], blue=[link.display])
+        return exchanges
 
     def _find_original_pid(self, relationships, trait, value):
         r_source = next((r.source for r in relationships if r.target == (trait, value)), None)
         if r_source:
-            if r_source[0] == 'host.process.id':
+            if r_source[0] == self.PID_FACT:
                 return int(r_source[1])
-            else:
-                return self._find_original_pid(relationships, r_source.trait, r_source.value)
+            return self._find_original_pid(relationships, r_source.trait, r_source.value)
         return 0
 
     @staticmethod
