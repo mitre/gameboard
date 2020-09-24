0c97cc05fe4696d438672207bae7d7c6b492ac6a
Govardhen Arunagiri <garunagiri@mitre.org>
2020-09-24 13:26:45 -0400

    making hunt abilities and adversary hidden

--- hook.py
+++ hook.py
@@ -1,3 +1,6 @@
+import glob
+import os
+
 from app.utility.base_world import BaseWorld
 from plugins.gameboard.app.gameboard_api import GameboardApi
 from plugins.gameboard.app.gameboard_svc import GameboardService
@@ -18,3 +21,22 @@ async def enable(services):
     app.router.add_route('PUT', '/plugin/gameboard/pin', gameboard_api.update_pin)
     app.router.add_route('POST', '/plugin/gameboard/hidden', gameboard_api.create_hidden_red_operation)
     app.router.add_route('POST', '/plugin/gameboard/detection', gameboard_api.verify_detection)
+
+
+async def expansion(services):
+    data_svc = services.get('data_svc')
+    await _apply_hidden_access_to_loaded_files(data_svc)
+
+
+async def _apply_hidden_access_to_loaded_files(data_svc):
+    object_types = ['abilities', 'adversaries']
+    for obj_typ in object_types:
+        for filename in glob.iglob('plugins/gameboard/data/'+obj_typ+'/**/*.yml', recursive=True):
+            obj_id = os.path.splitext(os.path.basename(filename))[0]
+            if obj_typ == 'abilities':
+                match = dict(ability_id=obj_id)
+            else:
+                match = dict(adversary_id=obj_id)
+            objects = await data_svc.locate(obj_typ, match=match)
+            for obj in objects:
+                obj.access = BaseWorld.Access.HIDDEN
